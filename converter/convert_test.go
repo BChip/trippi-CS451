970036a9718985ed15b3914dcbcd6ee1259bae5c
Chip <bachippi@svsu.edu>
2017-03-24 20:44:21 +0000

    changed how input looks in convert_test.go

--- converter/convert_test.go
+++ converter/convert_test.go
@@ -8,7 +8,15 @@ import (
 
 func TestConverter(t *testing.T) {
 
-	input := "let numOne = 5; let numTwo = 10; let sum = numOne + numTwo; print(\"The sum of num1 and num2 is: \"+sum); if(sum > 20){ print(\"The sum is greater than 20\"); }"
+	input := `
+	let numOne = 5; 
+	let numTwo = 10; 
+	let sum = numOne + numTwo; 
+	print("The sum of num1 and num2 is: "+sum); 
+	if(sum > 20){ 
+		print("The sum is greater than 20"); 
+	}
+	`
 
 	l := lexer.New(input)
 	c := New(l)
